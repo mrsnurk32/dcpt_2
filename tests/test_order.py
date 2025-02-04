e979e402842903794774066d849b7c650f07d775
Nick <nickbelik@mail.ru>
2025-02-04 17:40:15 +0300

    code validation amended

--- tests/test_order.py
+++ tests/test_order.py
@@ -8,3 +8,9 @@ def test_order():
     assert response.status_code == 200
     data = response.json()
     assert data['code'] == "212"
+
+def test_order_small_code():
+    response = client.post("/table_order/", json={"operation": "order", "table": 1, "waiter": 1})
+    assert response.status_code == 200
+    data = response.json()
+    assert data['code'] == "201"
